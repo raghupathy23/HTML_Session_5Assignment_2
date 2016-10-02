9e4222e5166417429816b3b775991be18283cde3
raghupathy23 <raghupathy0123@gmail.com>
2016-10-02 19:22:32 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+	var person = {
+		name: "Raghu",
+		age: 28,
+		dateofbirth: '23 Jan 1988',
+		placeofbirth: "COIMBATORE"
+	}
+	
+  console.log(person);
+	
+  // To display individual value of property using Dot Operator
+	console.log("Name: " +person.name);
+	console.log("Age: " +person.age);
+	console.log("Date of Birth: " +person.dateofbirth);
+	console.log("Place of Birth: " +person.placeofbirth);
